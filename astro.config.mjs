c9fa0ef5c9867e2bcf9f34c7de5b5251b005d522
Digissential <digissentialteam@gmail.com>
2025-09-09 10:45:50 +0200

    build: remove netlify adapter for static deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://digissential.co.za",
+  site: process.env.PUBLIC_SITE_URL ?? "https://digissential.co.za",
   trailingSlash: "always",
   output: "static",
-  adapter: netlify(),
   integrations: [tailwind({ applyBaseStyles: true })],
   build: { outDir: "dist" },
 });
